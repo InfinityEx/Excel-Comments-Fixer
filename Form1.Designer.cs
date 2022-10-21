6a08c0ada7d8c4eed6e737028127a681b9b00b54
InfinityEx <756423078@qq.com>
2022-10-22 00:10:19 +0800

    upload source code in 2019

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Excel_Comments_Fixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(204, 219);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "修复Excel批注\nFix Excel Document";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("楷体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "说明：";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(30, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(639, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "1.目前仅支持修复Excel2007/2010以上版本文件批注不正确的问题";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(30, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(459, 28);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "2.点击“一键修复注释”开始修复Excel表格批注";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(30, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(617, 28);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "3.为避免修复失败造成原文件损坏，请对备份后的文件进行修复！";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(363, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 44);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "关于";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(204, 343);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 44);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "注意事项";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(204, 285);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 50);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "批量修复\n暂不可用";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Control;
+            this.button5.Enabled = false;
+            this.button5.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button5.Location = new System.Drawing.Point(363, 285);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 49);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "批量删除\n(暂不可用)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label5.Location = new System.Drawing.Point(33, 401);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(281, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Zip DLL:icsharpcode/SharpZipLib\nCopyright © 2000-2018 SharpZipLib Contributors";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 434);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel批注修复工具_V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.IO.Compression;
+using System.IO;
+using System.Drawing.Drawing2D;
+using CZiplib = ICSharpCode.SharpZipLib;
+using CZip = ICSharpCode.SharpZipLib.Zip;
+using CZipSum =ICSharpCode.SharpZipLib.Checksum;
+using System.Threading;
+
+namespace Excel_Comments_Fixer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            OpenFileDialog path = new OpenFileDialog();
+            //文件类型过滤
+            path.Filter = @"待修复Excel文档|*.xlsx|待修复带有宏的Excel文档|*.xlsm";
+            //展开文件选择对话框
+            DialogResult result = path.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                //获取导出文件夹目录
+                string expath = path.FileName + @"_Fix";
+                if (Directory.Exists(expath))
+                {
+                    DialogResult fg = MessageBox.Show("导出数据已存在！是否覆盖？覆盖后将失去所有已导出数据！", "警告", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (fg == DialogResult.Yes)//确认删除
+                    {
+                        //对重复文件夹进行删除操作
+                        DirectoryInfo dl = new DirectoryInfo(expath);
+                        dl.Delete(true);
+                        DialogResult qr = MessageBox.Show("删除命令已执行，是否继续？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (qr == DialogResult.Yes)
+                        {
+                            //继续执行命令
+                        }
+                        else
+                        {
+                            if (qr == DialogResult.No)//删除文件夹后不继续
+                            {
+                                i = 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (fg == DialogResult.No)//确认不删除
+                        {
+                            i = 1;
+                        }
+                    }
+                }
+                //强制用户确认后才进行解压操作
+                if (i == 0)
+                {
+                    //获取打开文件的路径（全路径，含文件名）
+                    int fnlength = path.ToString().LastIndexOf("FileName");
+                    string sFile = path.ToString().Substring(fnlength + 9, path.ToString().Length - fnlength - 9);
+                    //获取打开文件所在目录，传递至sourcepath
+                    string sourcepath = Path.GetDirectoryName(sFile);
+
+                    //开始解压文件
+                    ZipFile.ExtractToDirectory(path.FileName, expath);
+                    //跳转至xl文件夹下
+                    string copath = expath + "\\xl\\";
+                    //获取xl文件夹下所有带有comments字符的xml文件路径
+                    string[] pathFile = Directory.GetFiles(copath, "comments*.xml", SearchOption.TopDirectoryOnly);
+                    //将中间文件命名为cobak.xml
+                    string strcon = copath + @"cobak.xml";
+                    //暂时将替换容器内容清空
+                    string con = "";
+                    foreach (string str in pathFile)
+                    {
+                        StreamReader reader = new StreamReader(str, Encoding.UTF8);
+                        //读取至文件尾
+                        con = reader.ReadToEnd();
+                        //替换shapeId="0"为空值
+                        con = con.Replace(@"shapeId=""0""", "");
+                        //替换Excel2013的不正确的XML代码块
+                        con = con.Replace(@"</commment></commentList></comments>", "");
+
+                        //开始以UTF-8编码开始写操作
+                        StreamWriter writer = new StreamWriter(strcon, false, Encoding.UTF8);
+                        //写入已替换的内容
+                        writer.Write(con);
+                        //释放写入缓冲区，暂时关闭写入器
+                        writer.Flush();
+                        writer.Close();
+                        reader.Close();
+
+                        //将写入文件复制为原文件，删除写入文件
+                        File.Copy(strcon, str, true);
+                        File.Delete(strcon);
+                    }
+                    //到此替换写入操作已完成
+                    //打开Excel文件和注释所在路径
+                    //System.Diagnostics.Process.Start("explorer.exe", sourcepath);
+                    //System.Diagnostics.Process.Start("explorer.exe", copath);
+
+                    //开始修改文件扩展名
+                    //Path.GetExtension(sFile);
+                    //Path.GetFileName(sFile);
+                    string nFile = Path.ChangeExtension(sFile, ".zip");
+                    FileInfo fi = new FileInfo(sFile);
+                    fi.MoveTo(nFile);
+
+
+
+                    //MessageBox.Show(nFile, "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    CZip.ZipFile zip = new CZip.ZipFile(nFile);
+                    string list = string.Empty;
+                    foreach (CZip.ZipEntry entry in zip)
+                    {
+                        list += entry.Name + "\r\n";
+                    }
+                    //MessageBox.Show(list);
+                    zip.BeginUpdate();
+                    foreach (string str in pathFile)
+                    {
+                        string filename = Path.GetFileName(str);
+                        zip.Add(str, @"/xl/" + filename);
+                        //MessageBox.Show(@"/xl/" + filename, "T", MessageBoxButtons.OK, MessageBoxIcon.None);
+                    }
+                    zip.CommitUpdate();
+                    zip.Close();
+                    string endFile = Path.ChangeExtension(nFile, ".xlsx");
+                    fi.CopyTo(endFile, true);
+                    DirectoryInfo dl = new DirectoryInfo(expath);
+                    dl.Delete(true);
+                    File.Delete(nFile);
+
+                    /*DirectoryInfo dl = new DirectoryInfo(expath);
+                    dl.Delete(true);*/
+
+
+                    System.Diagnostics.Process.Start("explorer.exe", sourcepath);
+                }
+            }
+        }
+
+        /*废弃代码存放处
+        //MessageBox.Show(path.ToString().Length.ToString(), "info", MessageBoxButtons.OK, MessageBoxIcon.None);
+        // string sourcepath = path.FileName.ToString();
+        //MessageBox.Show(sourcepath, "source_path", MessageBoxButtons.OK, MessageBoxIcon.None);
+        //string afterpath = path + @"Fixer.rar";
+        //MessageBox.Show("文件夹已存在！是否覆盖？覆盖后将失去已解压的所有数据！", "警告", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+        //MessageBox.Show("删除命令已执行，是否继续？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        //MessageBox.Show(copath, "copath", MessageBoxButtons.OK, MessageBoxIcon.None);
+        //
+        //int[] a = new int[100];
+        //string strFilePath = copath;
+        //string strIndex= @"shapeId=""0""";
+        //MessageBox.Show(strIndex, "info", MessageBoxButtons.OK, MessageBoxIcon.None);
+        //string newValue="";
+        //{
+            if (System.IO.File.Exists(strFilePath))
+            {
+                string[] lines = System.IO.File.ReadAllLines(strFilePath);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].Contains(strIndex))
+                    {
+                        string[] str = lines[i].Split('=');
+                        str[1] = newValue;
+                        lines[i] = str[0] + " = " + str[1];
+                    }
+                }
+                System.IO.File.WriteAllLines(strFilePath, lines);
+            }
+        }
+        string oldString;
+        string newString = "";
+        FileStream fs = new FileStream(allpath,FileMode.Open,FileAccess.ReadWrite);
+        StreamReader sr = new StreamReader(fs, Encoding.Default);
+        oldString = sr.ReadToEnd();
+        newString = oldString.Replace(@"shapeId=""0""", newString);
+        sr.Close();
+        StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+        sw.Flush(); //清除流内容
+        sw.Write(newString); //重写替换内容后的字符串
+        sw.Close();
+        fs.Close();
+        // int i = 1;
+        // string a = i.ToString();
+        // string endpath = "*.xml";
+        // string allpath = copath + endpath;
+        //
+        //string aapath = allpath;
+                        FileStream fs = new FileStream(str, FileMode.Open, FileAccess.ReadWrite);
+                        MessageBox.Show(str, "str", MessageBoxButtons.OK, MessageBoxIcon.None);
+                        StreamReader sr = new StreamReader(fs);
+                        con = sr.ReadToEnd();
+                        MessageBox.Show(con, "con_read_file_until_end", MessageBoxButtons.OK, MessageBoxIcon.None);
+                        con = con.Replace(@"shapeId=""0""", "");
+                        con = con.Replace(@"</commment></commentList></comments>", "");
+                        MessageBox.Show(con, "con_after_trans", MessageBoxButtons.OK, MessageBoxIcon.None);
+                        sr.Close();
+                        fs.Close();
+                        FileStream fs2 = new FileStream(str, FileMode.OpenOrCreate, FileAccess.Write);
+                        MessageBox.Show(str, "fs2_str", MessageBoxButtons.OK, MessageBoxIcon.None);
+                        StreamWriter sw = new StreamWriter(fs2,append:false);
+                        sw.WriteLine(con);
+                        MessageBox.Show(con, "fs2_write_con", MessageBoxButtons.OK, MessageBoxIcon.None);
+                        sw.Close();
+                        fs2.Close();
+        //FileStream fs = new FileStream(str, FileMode.Open, FileAccess.ReadWrite);
+        //MessageBox.Show(con, "con_del_shapeId", MessageBoxButtons.OK, MessageBoxIcon.None);
+        //MessageBox.Show(path.ToString(), "con_del_comment", MessageBoxButtons.OK, MessageBoxIcon.None);
+        //MessageBox.Show(copath, "comments_copath", MessageBoxButtons.OK, MessageBoxIcon.None);
+        //MessageBox.Show(con, "con_del_comment", MessageBoxButtons.OK, MessageBoxIcon.None);
+        //File.Copy(sourcepath, afterpath, true);
+        
+            //原button2_code
+            DialogResult ztx = MessageBox.Show("请确认已将指定文件复制到压缩包内！继续请点是，其他请点否", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (ztx == DialogResult.Yes)
+                {
+                OpenFileDialog transpath = new OpenFileDialog();
+                //文件类型过滤
+                transpath.Filter = @"待转化压缩包|*.xlsx";
+                DialogResult resultt = transpath.ShowDialog();
+                if (resultt == DialogResult.OK)
+                {
+                    //获取打开文件的路径（全路径，含文件名）
+                    int fnlength2 = transpath.ToString().LastIndexOf("FileName");
+                    string sFile2 = transpath.ToString().Substring(fnlength2 + 9, transpath.ToString().Length - fnlength2 - 9);
+                    //开始修改文件扩展名
+                    Path.GetExtension(sFile2);
+                    string dd = Path.GetExtension(sFile2);
+                    Path.ChangeExtension(sFile2, "xlsx");
+                    string fFile = Path.ChangeExtension(sFile2, ".xlsx");
+                    FileInfo fn = new FileInfo(sFile2);
+                    //fn.MoveTo(fFile);
+
+                    string directory = "\\xl";
+                    string expath = transpath.FileName + @"_Fix";
+                    string copath = expath + "\\xl\\";
+                    string[] pathFile = Directory.GetFiles(copath, "comments*.xml", SearchOption.TopDirectoryOnly);
+
+                    try
+                    {
+                        CZip.ZipFile zip = new CZip.ZipFile(sFile2);
+                        //zip.AddDirectory(pathFile,);
+                        zip.BeginUpdate();
+                        foreach (string str in pathFile)
+                        {
+
+                        }
+                        zip.CommitUpdate();
+                    }
+                    catch(Exception ex)
+                    {
+                        throw ex;
+                    }
+                    MessageBox.Show("恢复完成！请使用Excel2007打开文件！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                   
+
+            }
+           */
+
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        //关于
+        {
+            MessageBox.Show("程序使用C#编写\n(需要安装.Net Framework 4.5环境才可正常运行)\nZip功能来自icsharpcode/SharpZipLib开源项目,项目作者及贡献者版权所有" +
+                "\n支持修复注释的版本:Excel2013\n(Excel2016修复一次后仍需要Excel程序再次修复)\nBy Raven\n\n" +
+                "历史更新:" +
+                "\n(2019.10.16)修复程序逻辑性bug" +
+                "\n(2019.10.17)修正替换内容后的代码遗留问题" +
+                "\n(2019.10.18)修复<一键修复>功能存在的功能性错误" +
+                "\n(2020.11.03)修正描述错误,修正由于疏漏没有xlsm格式可选的问题",
+                "关于Excel批注修复工具", MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("暂不支持对Excel2016以上版本xlsx文件进行批注修复", "注意事项", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Multiselect = true;//等于true表示可以选择多个文件
+            dlg.DefaultExt = ".xlsx";
+            dlg.Filter = "待删除批注的Excel文档|*.xlsx|待删除批注带有宏的Excel文档|*.xlsm";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+
+                foreach (string file in dlg.FileNames)
+                {
+                    MessageBox.Show(file);
+                    string sfile = Path.ChangeExtension(file, ".zip");
+                    FileInfo fi = new FileInfo(file);
+                    fi.MoveTo(sfile);
+                    CZip.ZipFile zip = new CZip.ZipFile(sfile);
+                    zip.BeginUpdate();
+                    zip.Delete(@"xl\comments*.xml");
+                    zip.CommitUpdate();
+                }
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+        
+
